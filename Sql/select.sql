4ec0a77176fdf71a98706feae417e63c1fd6432f
Hannah <lilypickle@hotmail.fr>
2021-01-07 23:23:17 +0100

    Select queries on tables for database

---
+++ Sql/select.sql
@@ -0,0 +1,31 @@
+USE book_list;
+
+SELECT * FROM Binding_Details;
+SELECT * FROM Genre;
+SELECT * FROM Languages;
+SELECT * FROM Nationalites;
+SELECT * FROM City;
+SELECT * FROM Postal_code;
+SELECT * FROM Currency;
+SELECT * FROM Gender;
+SELECT * FROM Author_Details;
+SELECT * FROM Purchase;
+SELECT * FROM Address;
+SELECT * FROM Book_Details;
+SELECT * FROM WishList;
+SELECT * FROM Owned;
+SELECT * FROM Lost;
+SELECT * FROM Reader;
+SELECT * FROM Shop;
+SELECT * FROM In_book_Shelf;
+SELECT * FROM isInCategory;
+SELECT * FROM isWrittenIn;
+SELECT * FROM owns;
+SELECT * FROM purchasedFromShop;
+SELECT * FROM shopPurchased;
+SELECT * FROM readerPurchased;
+SELECT * FROM hasFavoriteGenre;
+
+
+
+
