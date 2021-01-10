9144666be5f9058a80eaa68ac701db45d8d9e9bc
Hannah <lilypickle@hotmail.fr>
2021-01-11 00:32:04 +0100

    updates procedure and call procedure. Problem with call addBook fixed

--- Sql/callProcedure.sql
+++ Sql/callProcedure.sql
@@ -1,4 +1,6 @@
 USE book_List;
 
 CALL count_books_per_categorie();
-CALL add_book("Harry Potter and the prisonner of Azkaban", 9781408855652, 1999-07-08, 1, 1, true);
+CALL add_book("Harry Potter and the prisonner of Azkaban", 0747574499, 1999-07-08, 1, 1);
+
+#If isbn has 0 at start creates a problem
--- Sql/procedures.sql
+++ Sql/procedures.sql
@@ -5,14 +5,14 @@ DELIMITER $$
 CREATE PROCEDURE count_books_per_categorie()
 BEGIN
 	SELECT 
-	(SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 1) AS "Romance" ,
-	(SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 2) AS "Horror" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 3) AS "Sci-Fi" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 4) AS "History" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 5) AS "Biography" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 6) AS "Auto-biography" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 7) AS "Young adults" ,
-    (SELECT COUNT(*) FROM book_list.isincategory WHERE Id_Genre = 8) AS "Children" 
+	(SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 1) AS "Romance" ,
+	(SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 2) AS "Horror" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 3) AS "Sci-Fi" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 4) AS "History" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 5) AS "Biography" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 6) AS "Auto-biography" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 7) AS "Young adults" ,
+    (SELECT COUNT(*) FROM book_list.isInCategory WHERE Id_Genre = 8) AS "Children" 
 	FROM book_list.isincategory
 	LIMIT 1;
 END $$
@@ -25,16 +25,14 @@ CREATE PROCEDURE add_book(
         IN ISBN_p INT,
         IN DatePublished_p Date,
         IN Id_Binding_p INT,
-        IN Id_Author_p INT,
-        IN Favorite_Book_p BOOLEAN)
-BEGIN
-		DECLARE Title_p VARCHAR(100);
-        DECLARE ISBN_p, Id_Binding_p, Id_Author_p INT;
-        DECLARE DatePublished_p Date;
+        IN Id_Author_p INT)
+BEGIN			
         IF (Title_p IS NOT NULL) THEN
-			INSERT INTO book_list.Book_Details VALUES (NULL, Title_p, ISBN_p, DatePublished_p, Favorite_Book_p, Id_Binding_p, Id_Author_p);
+			INSERT INTO book_list.Book_Details VALUES (NULL, Title_p, ISBN_p, DatePublished_p, Id_Binding_p, Id_Author_p);
 		ELSE 
 			SELECT "A book must have a title" AS ERROR;
         END IF;
 END $$
 DELIMITER ;
+
+
